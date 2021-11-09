563f7f47ca6db81f34163c79dc7bd8b8e687df8d
우종원 <93258739+Ubermensch0608@users.noreply.github.com>
2021-11-09 23:59:35 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const number = document.getElementById("number")
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+
+increase.onclick = () => {
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current + 1;
+}
+decrease.onclick = () => {
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current - 1;
+}
+reset.onclick = () => {
+    const current = parseInt(number.innerText, 10);
+    number.innerText = current;
+}
